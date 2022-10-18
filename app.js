49b4a909538b94c00e3f27ec56a57480dde6b2b4
Ricardo Perez <ricardo.perez@teksi.mx>
2022-10-18 16:35:54 -0500

    .

--- app.js
+++ app.js
@@ -20,52 +20,114 @@ paypal.configure({
         "EJYJc4U8xO3LIei9sVTYyMf3DnsQVqRrSiCm-SxOvtMe8vvmycCOWPdffPhmpi7sPYvexyEhCaKcE1QW"
 });
 
-app.get("/", (req, res) => {
-    res.render("index");
-});
+app.post('/api/pru/', function (req, res) {
 
-app.get("/paypal", (req, res) => {
-    let create_payment_json = {
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"
-        },
-        "redirect_urls": {
-            "return_url": "http://192.168.0.159:3000/success", //Ponga su localhost personal o host en la nube
-            "cancel_url": "http://192.168.0.159:3000/cancel"   //Ponga su localhost personal o host en la nube
-        },
-        "transactions": [
-            {
-                "item_list": {
-                    "items": [
-                        {
-                            "name": "item",
-                            "sku": "item",
-                            "price": "100.00",
-                            "currency": "MXN",
-                            "quantity": "1"
-                        }
-                    ]
-                },
-                "amount": {
-                    "currency": "MXN",
-                    "total": "100.00"
-                },
-                "description": "Descripcion de la compra."
-            }
-        ]
-    };
-
-    paypal.payment.create(create_payment_json, function (error, payment) {
-        if (error) {
-            throw error;
-        } else {
-            console.log("Create Payment Response");
-            console.log(payment);
-            res.redirect(payment.links[1].href);
-        }
+    console.log(req.body);
+    app.get("/", (req, res) => {
+        // console.log("_____________________________", req.params)
+        res.render("index");
     });
+
+    app.get("/paypal", (reqe, res) => {
+        let create_payment_json = {
+            "intent": "sale",
+            "payer": {
+                "payment_method": "paypal"
+            },
+            "redirect_urls": {
+                "return_url": "http://192.168.1.171:3000/success", //Ponga su localhost o host en la nube
+                "cancel_url": "http://192.168.1.171:3000/cancel"   //Ponga su localhost o host en la nube
+            },
+            "transactions": [
+                {
+                    "item_list": {
+                        "items": [
+                            {
+                                "name": req.body.name,
+                                "sku": req.body.sku,
+                                "price": req.body.price,
+                                "currency": req.body.currency,
+                                "quantity": req.body.quantity
+                            }
+                        ]
+                    },
+                    "amount": {
+                        "currency": req.body.currency,
+                        "total": "100.00"
+                    },
+                    "description": "Descripcion de la compra."
+                }
+            ]
+        };
+
+
+        paypal.payment.create(create_payment_json, function (error, payment) {
+            if (error) {
+                throw error;
+            } else {
+                console.log("Create Payment Response");
+                console.log(payment);
+                res.redirect(payment.links[1].href);
+            }
+        });
+    });
+
 });
+
+//app.post("/api/Paypal", (req, res) => {
+
+// app.get("/", (req, res) => {
+//     console.log("_____________________________", req.params)
+//     res.render("index");
+// });
+
+// app.get("/paypal", (req, res) => {
+//     let create_payment_json = {
+//         "intent": "sale",
+//         "payer": {
+//             "payment_method": "paypal"
+//         },
+//         "redirect_urls": {
+//             "return_url": "http://192.168.1.171:3000/success", //Ponga su localhost o host en la nube
+//             "cancel_url": "http://192.168.1.171:3000/cancel"   //Ponga su localhost o host en la nube
+//         },
+//         "transactions": [
+//             {
+//                 "item_list": {
+//                     "items": [
+//                         {
+//                             "name": "item",
+//                             "sku": "item",
+//                             "price": "100.00",
+//                             "currency": "MXN",
+//                             "quantity": "1"
+//                         }
+//                     ]
+//                 },
+//                 "amount": {
+//                     "currency": "MXN",
+//                     "total": "100.00"
+//                 },
+//                 "description": "Descripcion de la compra."
+//             }
+//         ]
+//     };
+
+
+//     paypal.payment.create(create_payment_json, function (error, payment) {
+//         if (error) {
+//             throw error;
+//         } else {
+//             console.log("Create Payment Response");
+//             console.log(payment);
+//             res.redirect(payment.links[1].href);
+//         }
+//     });
+// });
+//});
+
+
+
 
 app.get("/success", (req, res) => {
     // res.send("Success");
